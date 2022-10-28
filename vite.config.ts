fa1453684905430f3dd6f87439b36bc97c02707f
A-Emile <a-emile-g@protonmail.com>
2022-10-28 16:04:27 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { join } from "path";
+import { fileURLToPath } from "url";
 
 export default defineConfig(() => {
   return {
+    build: {
+      outDir: join(fileURLToPath(import.meta.url), '..', '..', 'dist'),
+    },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
   };
 });
