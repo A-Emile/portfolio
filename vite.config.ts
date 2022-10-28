94d8a1976a8249192f551eefc7673997cbe69d63
A-Emile <a-emile-g@protonmail.com>
2022-10-28 16:15:30 +0200

    removed unused imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { join } from "path";
-import { fileURLToPath } from "url";
 
 export default defineConfig(() => {
   return {
