ef9668b4da21abd490fcc7ca9e1900e4f382307e
A-Emile <a-emile-g@protonmail.com>
2022-10-28 15:49:21 +0200

    test

--- adaptors/static/vite.config.ts
+++ adaptors/static/vite.config.ts
@@ -1,12 +1,14 @@
 import { staticAdaptor } from '@builder.io/qwik-city/adaptors/static/vite';
 import { extendConfig } from '@builder.io/qwik-city/vite';
 import baseConfig from '../../vite.config';
+import { fileURLToPath } from 'node:url';
+import { join } from 'node:path';
 
 export default extendConfig(baseConfig, () => {
   return {
     build: {
       ssr: true,
-      outDir: "dist",
+      outDir: join(fileURLToPath(import.meta.url), '..', '..', 'dist'),
       rollupOptions: {
         input: ['@qwik-city-plan'],
       },
