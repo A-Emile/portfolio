46796e54860453109a592c590170059046679392
A-Emile <a-emile-g@protonmail.com>
2022-11-01 20:13:46 +0100

    Temporarily changed nav links to normal html links cause of bug with useServerMount

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,14 @@
 import { component$ } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
 
   return (
     <header>
       <nav className='p-5 border-b gap-5 items-center px-7 border-gray-800 flex'>
-        <Link className='font-bold text-2xl flex-grow' href='/'>A-Emile</Link>
-        <Link className='underline-offset-2 hover:underline' href='/projects'>Projects</Link>
+        <a className='font-bold text-2xl flex-grow' href='/'>A-Emile</a>
+        <a className='underline-offset-2 hover:underline' href='/projects'>Projects</a>
         <a className='underline-offset-2 hover:underline' href='/blog'>Blog</a>
-        <Link className='underline-offset-2 hover:underline' href='/contact'>Contact</Link>
+        <a className='underline-offset-2 hover:underline' href='/contact'>Contact</a>
       </nav>
     </header>
   );
