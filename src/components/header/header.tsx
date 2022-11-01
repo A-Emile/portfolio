abf0d1c3c73f4f0fb0d76d27e76b965b7fb70655
A-Emile <a-emile-g@protonmail.com>
2022-11-01 22:38:52 +0100

    Improved navbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,11 +4,11 @@ export default component$(() => {
 
   return (
     <header>
-      <nav className='p-5 border-b gap-5 items-center px-7 border-gray-800 flex'>
+      <nav className='p-5 border-b items-center px-7 border-gray-800 flex'>
         <a className='font-bold text-2xl flex-grow' href='/'>A-Emile</a>
-        <a className='underline-offset-2 hover:underline' href='/projects'>Projects</a>
-        <a className='underline-offset-2 hover:underline' href='/blog'>Blog</a>
-        <a className='underline-offset-2 hover:underline' href='/contact'>Contact</a>
+        <a className='transition-colors rounded-lg p-1 font-semibold text-gray-400 hover:bg-back2 px-5' href='/'>Home</a>
+        <a className='transition-colors rounded-lg p-1 font-semibold text-gray-400 hover:bg-back2 px-5' href='/blog'>Blog</a>
+        <a className='transition-colors rounded-lg p-1 font-semibold text-gray-400 hover:bg-back2 px-5' href='/contact'>Contact</a>
       </nav>
     </header>
   );
