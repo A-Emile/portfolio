a58f58d49ad825e64f53f992e3d4c52250454621
A-Emile <a-emile-g@protonmail.com>
2022-11-07 21:14:50 +0100

    Added e-commerce project and improved theme

--- src/components/Projects/ProjectList.tsx
+++ src/components/Projects/ProjectList.tsx
@@ -4,11 +4,11 @@ import { ProjectCard } from "./ProjectCard"
 export default component$(() => {
     return (
         <>
-            <h1 className="text-lg font-semibold mb-3">Projects</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 animate__animated animate__fadeIn">
-                <ProjectCard gradient="from-blue-700 to-blue-200" href="/projects/clonlio" img="/projects/clonlio/preview.png" title={"Clonlio"} desc="Portfolio system for Clonlara Students" />
+            <h1 className="mb-3 text-lg font-semibold">Projects</h1>
+            <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 animate__animated animate__fadeIn">
+                <ProjectCard gradient="from-fuchsia-700 to-fuchsia-400" href="/projects/ecomm" img="/projects/ecomm/preview.png" title="E-Commerce Website" desc="Landing page for WAKE-MT-2" />
+                <ProjectCard gradient="from-blue-700 to-blue-200" href="/projects/clonlio" img="/projects/clonlio/preview.png" title={"Clonlio"} desc="Portfolio management system for Clonlara Students" />
                 <ProjectCard gradient="from-purple-600 to-[#ff0080]" href="/projects/zauberhafte-welten" img="/projects/zw/preview.jpg" title={"Zauberhafte Welten"} desc="Website for Zauberhafte Welten" />
-                <ProjectCard gradient="from-yellow-500 to-pink-300"  href="/projects/portfolio" img="/projects/pp/preview.png" title={"Personal Portfolio"} desc="Personal Portfolio Website" />
             </div>
         </>
     )
---
+++ src/routes/projects/ecomm/index.md
@@ -0,0 +1,7 @@
+---
+title: Portfolio
+---
+
+![Cover image](/projects/ecomm/cover.png)
+# [E-Commerce Website](https://github.com/A-Emile/gatsby-starter-default)
+Modelled a wakeboard mount and designed a landing page.
