7dbc4fc9b1f7fadc00a91a357e1632699b244b69
A-Emile <a-emile-g@protonmail.com>
2022-11-22 12:48:51 +0100

    Changed language to german, added technologies

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,10 +3,10 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
 
   return (
-    <footer className='border-back2 border-t text-gray-500 flex justify-center items-center p-5'>
+    <footer className='flex justify-center items-center p-5 text-gray-500 border-t border-back2'>
       <div>
         <a href="https://github.com/A-Emile" target="_blank">
-          Made with <font className='text-red-500'>♡</font> by A-Emile
+          Made with <font className='text-red-500'>♡</font> by Anatole Gonon
         </a>
       </div>
     </footer>
